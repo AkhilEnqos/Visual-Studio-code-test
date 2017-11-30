06acd3dbad9f30ee421fc9f01a60d0f9be5482ef
Akhil <akhilt@enqos.com>
2017-11-30 18:38:18 +0530

    package.json and nodemon

--- public/routing/app.js
+++ public/routing/app.js
@@ -24,5 +24,5 @@ var server = http.createServer(function(req, res){
     }
 });
 
-server.listen(3000, '127.0.0.1');
-console.log('Now listening to port 3000');
+server.listen(3001, '127.0.0.1');
+console.log('Now listening to port 3001');
