7efd1c5cbeaaaf96138f4c7f01123ffcdad23708
Akhil <akhilt@enqos.com>
2017-12-01 17:19:04 +0530

    Nodejs update

---
+++ public/express-test/app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req,res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/contact', function(req,res){
+    res.send('this is the Contact Page');
+});
+
+app.get('/profile/:id', function(req,res){
+    res.send('You requested to see a profile with the id of ' + req.params.id);
+});
+
+
+
+app.listen(3000);
