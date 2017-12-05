8729589017d326e806ded4a09a226d4e8ef964be
Akhil <akhilt@enqos.com>
2017-12-05 16:04:09 +0530

    Update

--- public/express-test/app.js
+++ public/express-test/app.js
@@ -2,12 +2,14 @@ var express = require('express');
 
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req,res){
     res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/contact', function(req,res){
-    res.send('this is the Contact Page');
+    res.sendFile(__dirname + '/contact.html');
 });
 
 app.get('/profile/:id', function(req,res){
